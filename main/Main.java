5700ecbc71363af691d7fdefa92e000bd39b925e
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-08-01 10:54:51 +0530

    checking happy or sad

--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
 public class Main {
+	public static void main(String[] args) {
+		MoodFind moodAnalyser = new MoodFind();
+		System.out.println(moodAnalyser.analyseMood("happy"));
+		System.out.println(moodAnalyser.analyseMood("sad"));
+		System.out.println(moodAnalyser.analyseMood("normal"));
 
+	}
 }
--- main/MoodFind.java
+++ main/MoodFind.java
@@ -1,5 +1,32 @@
 package main;
 
-public class MoodFind {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class MoodFind {
+	public String analyseMood(String str) {
+		int flag1=0,flag2=0;
+		Pattern pattern =Pattern.compile("happy|Happy");
+		Matcher matcher=pattern.matcher(str);
+		while(matcher.find()) {
+			flag1=1;
+		}
+		
+		pattern =Pattern.compile("sad|Sad");
+		matcher=pattern.matcher(str);
+		while(matcher.find()) {
+			flag2=1;
+		}
+		
+		if(flag1==1) {
+			return "HAPPY";
+		}
+		else if(flag2==1) {
+			return "SAD";
+		}else {
+			return "HAPPY";
+		}
+		
+		
+	}
 }
